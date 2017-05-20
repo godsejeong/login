1e23c35bcf43e8cd4fd26fe763c3f620088b4c00
gotsejeong <dbdnpxm@naver.com>
2017-05-20 23:34:01 +0900

    login_from

---
+++ app/src/main/java/com/wark/login_2/login_2.java
@@ -0,0 +1,17 @@
+package com.wark.login_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mikhaellopez.circularimageview.CircularImageView;
+
+import static android.R.attr.shape;
+
+public class login_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_2);
+    }
+}
